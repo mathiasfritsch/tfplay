c321fa1e8801409b10a5c18e3e776a346d0c7a72
mathiasfritsch <mathias.fritsch.hh@gmail.com>
2025-12-31 17:07:59 +0100

    Implement Application Load Balancer and associated resources; configure security groups, subnets, and outputs for ALB DNS name.

--- main.tf
+++ main.tf
@@ -13,16 +13,40 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+resource "aws_security_group" "alb" {
+  name        = "alb-sg"
+  description = "Allow inbound HTTP traffic on port 80"
+
+  ingress {
+    description = "HTTP from internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
 resource "aws_security_group" "web_server" {
   name        = "web-server-sg"
   description = "Allow inbound traffic on port ${var.http_port}"
 
   ingress {
-    description = "HTTP on ${var.http_port}"
-    from_port   = var.http_port
-    to_port     = var.http_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description     = "HTTP on ${var.http_port} from ALB"
+    from_port       = var.http_port
+    to_port         = var.http_port
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
   }
 
   egress {
@@ -41,11 +65,48 @@ data "aws_vpc" "default" {
   default = true
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 data "aws_subnets" "default" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
+  
+  filter {
+    name   = "default-for-az"
+    values = ["true"]
+  }
+}
+
+resource "aws_subnet" "additional_1" {
+  vpc_id            = data.aws_vpc.default.id
+  cidr_block        = "172.31.48.0/20"
+  availability_zone = data.aws_availability_zones.available.names[1]
+  
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "additional-subnet-1"
+  }
+}
+
+resource "aws_subnet" "additional_2" {
+  vpc_id            = data.aws_vpc.default.id
+  cidr_block        = "172.31.64.0/20"
+  availability_zone = data.aws_availability_zones.available.names[2]
+  
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "additional-subnet-2"
+  }
+}
+
+locals {
+  all_subnet_ids = concat(data.aws_subnets.default.ids, [aws_subnet.additional_1.id, aws_subnet.additional_2.id])
 }
 
 resource "aws_launch_template" "web_server" {
@@ -78,12 +139,61 @@ resource "aws_launch_template" "web_server" {
   }
 }
 
+resource "aws_lb_target_group" "web_server" {
+  name     = "web-server-tg"
+  port     = var.http_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 30
+    matcher             = "200"
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 5
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "web-server-tg"
+  }
+}
+
+resource "aws_lb" "web_server" {
+  name               = "web-server-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = local.all_subnet_ids
+
+  tags = {
+    Name = "web-server-alb"
+  }
+}
+
+resource "aws_lb_listener" "web_server" {
+  load_balancer_arn = aws_lb.web_server.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_server.arn
+  }
+}
+
 resource "aws_autoscaling_group" "web_server" {
   name                = "web-server-asg"
   min_size            = 2
   max_size            = 4
   desired_capacity    = 2
-  vpc_zone_identifier = data.aws_subnets.default.ids
+  vpc_zone_identifier = local.all_subnet_ids
+  target_group_arns   = [aws_lb_target_group.web_server.arn]
+  health_check_type   = "ELB"
+  health_check_grace_period = 300
 
   launch_template {
     id      = aws_launch_template.web_server.id
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "asg_name" {
   description = "Name of the Auto Scaling Group"
   value       = aws_autoscaling_group.web_server.name
 }
+
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = aws_lb.web_server.dns_name
+}
